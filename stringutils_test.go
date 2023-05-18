81867a7708d224d6bdca7d490fd49b2610d6c07c
Umputun <umputun@gmail.com>
2023-05-18 03:14:34 -0500

    add basic set of string utils

---
+++ stringutils_test.go
@@ -0,0 +1,115 @@
+package stringutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		name string
+		src  string
+		list []string
+		want bool
+	}{
+		{"finds string", "test", []string{"test", "example"}, true},
+		{"doesn't find string", "missing", []string{"test", "example"}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, Contains(tt.src, tt.list))
+		})
+	}
+}
+
+func TestContainsAnySubstring(t *testing.T) {
+	tests := []struct {
+		name       string
+		str        string
+		substrings []string
+		want       bool
+	}{
+		{"contains substring", "hello world", []string{"world", "example"}, true},
+		{"doesn't contain substring", "hello world", []string{"missing", "example"}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, ContainsAnySubstring(tt.str, tt.substrings))
+		})
+	}
+}
+
+func TestDeDup(t *testing.T) {
+	tests := []struct {
+		name string
+		keys []string
+		want []string
+	}{
+		{"removes duplicates", []string{"test", "test", "example"}, []string{"test", "example"}},
+		{"no duplicates", []string{"test", "test2", "example"}, []string{"test", "test2", "example"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, DeDup(tt.keys))
+		})
+	}
+}
+
+func TestDeDupBig(t *testing.T) {
+	tests := []struct {
+		name string
+		keys []string
+		want []string
+	}{
+		{"removes duplicates", []string{"test", "test", "example"}, []string{"test", "example"}},
+		{"no duplicates", []string{"test", "test2", "example"}, []string{"test", "test2", "example"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, DeDupBig(tt.keys))
+		})
+	}
+}
+
+func TestSliceToString(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []interface{}
+		want []string
+	}{
+		{"converts number to string", []any{1, 2, 3}, []string{"1", "2", "3"}},
+		{"converts mixed slice to string", []any{1, "aaa", true, 0.55}, []string{"1", "aaa", "true", "0.55"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, SliceToString(tt.in))
+		})
+	}
+}
+
+func TestHasCommonElement(t *testing.T) {
+	cases := []struct {
+		name string
+		a, b []string
+		want bool
+	}{
+		{"element found", []string{"a", "b", "c", "d"}, []string{"x", "y", "z", "a"}, true},
+		{"element not found", []string{"a", "b", "c", "d"}, []string{"x", "y", "z", "w"}, false},
+		{"both slices are empty", []string{}, []string{}, false},
+		{"one slice is empty", []string{}, []string{"x", "y", "z", "w"}, false},
+		{"element found at the start", []string{"a", "b", "c", "d"}, []string{"a", "x", "y", "z"}, true},
+		{"element found at the end", []string{"a", "b", "c", "d"}, []string{"x", "y", "z", "d"}, true},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.want, HasCommonElement(tc.a, tc.b))
+		})
+	}
+}
